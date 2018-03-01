d107868028b7243a5b5db6122e839e164b342f67
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-01 15:12:11 -0600

    finished the exercise

---
+++ direct-exec.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+
+//  type the phrase 'direct-exec'
+// or type the phrase './direct-exec.js'
+
+// console.log('Hello World. I was executed without the node command');
+
+process.stdout.write('Hello World. I was executed without the node command\n');
